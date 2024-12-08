a59c70d5d0b4d366704a8639d14b4120090de459
Serhii <serhijkrasa64uk5@gmail.com>
2024-12-08 17:08:57 +0200

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -5,6 +5,32 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField("Actor", related_name="movies_actor")
+    genres = models.ManyToManyField("Genre", related_name="movies_genre")
 
     def __str__(self):
         return self.title
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=55)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
